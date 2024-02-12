3baec7c3d3975d48ed98d305a6fff35212ac4004
KokiLee <riko2501@gmail.com>
2024-02-12 18:25:31 +0900

    ロール補正修正

--- Assets/SerialHandler.cs
+++ Assets/SerialHandler.cs
@@ -144,6 +144,8 @@ public class SerialHandler : MonoBehaviour
                                 this.roll += 360;
                         }
                      }
+
+                    preRoll = this.roll;
                     
 
                     // Check
