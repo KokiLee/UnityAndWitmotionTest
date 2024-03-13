c603e71f6b77762a10f2443e9ab36806f0d7512e
KokiLee <riko2501@gmail.com>
2024-03-13 23:11:29 +0900

    modified dataparser

--- Assets/DataParser.cs
+++ Assets/DataParser.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System;
 using UnityEngine;
 
 public class DataParser : MonoBehaviour
@@ -10,9 +11,9 @@ public class DataParser : MonoBehaviour
     private double preRoll = 0;
     private double prePitch = 0;
     private double preYaw = 0;
-
+    private double testpreRoll = 0;
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         serialHandler.OnPortOpened += StartProcessingData;
     }
@@ -22,27 +23,25 @@ public class DataParser : MonoBehaviour
         StartCoroutine(ProcessDataCoroutine());
     }
 
-    void OnDestroy()
+    private void OnDestroy()
     {
         serialHandler.OnPortOpened -= StartProcessingData;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
     private static double AdjustData(double previous_data, double current_data)
     {
-
         double angleDiff = current_data - previous_data;
 
-        angleDiff = ((angleDiff + 180) % 360) - 180;
+        // Normalize angle difference to range from -180 degrees to 180 degrees.
+        angleDiff = (angleDiff + 180) % 360 - 180;
+        if (angleDiff < -180) angleDiff += 360; // Currently handle negative differences over -360 degrees.
 
+        // Update current angle using normalized difference
         current_data = previous_data + angleDiff;
 
-        current_data = ((current_data + 180) % 360) - 180;
+        // The current angle is also normalized to the range -180 degress to 180 degrees.
+        current_data = (current_data + 180) % 360 - 180;
+        if (current_data < -180) current_data += 360;
 
         return current_data;
     }
@@ -66,20 +65,33 @@ public class DataParser : MonoBehaviour
 
                 for (int i = 0; i < data.Length - 1; i++)
                 {
-                    if (data[i] == 0x55 && data[i + 1] == 0x53)
+                    if (data.Length >= 6)
                     {
-                        roll = ((data[i + 3] << 8) | data[i + 2]) / 32768.0 * 180.0;
-                        pitch = ((data[i + 5] << 8) | data[i + 4]) / 32768.0 * 180.0;
-                        yaw = ((data[i + 7] << 8) | data[i + 6]) / 32768.0 * 180.0;
+                        if (data[i] == 0x55 && data[i + 1] == 0x53)
+                        {
 
-                        roll = AdjustData(preRoll, roll);
-                        preRoll = roll;
+                            byte[] roll_L_H = { data[i + 2], data[i + 3] };
+                            short combined_roll = System.BitConverter.ToInt16(roll_L_H, 0);
 
-                        pitch = AdjustData(prePitch, pitch);
-                        prePitch = pitch;
+                            byte[] pitch_L_H = { data[i + 4], data[i + 5] };
+                            short combined_pitch = System.BitConverter.ToInt16(pitch_L_H, 0);
 
-                        yaw = AdjustData(preYaw, yaw);
-                        preYaw = yaw;
+                            byte[] yaw_L_H = { data[i + 6], data[i + 7] };
+                            short combined_yaw = System.BitConverter.ToInt16(yaw_L_H, 0);
+
+                            roll = combined_roll / 32768.0 * 180.0;
+                            pitch = combined_pitch / 32768.0 * 180.0;
+                            yaw = combined_yaw / 32768.0 * 180.0;
+
+                            roll = AdjustData(preRoll, roll);
+                            preRoll = roll;
+
+                            pitch = AdjustData(prePitch, pitch);
+                            prePitch = pitch;
+
+                            yaw = AdjustData(preYaw, yaw);
+                            preYaw = yaw;
+                        }
                     }
                 }
             }
